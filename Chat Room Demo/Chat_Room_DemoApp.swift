33d916cabd2a7f677d21eb1baa6f33ca99cd7885
Mai Quang Linh <mai.quang.linh.hl97@gmail.com>
2021-11-07 14:37:52 +0900

    Initial Commit

---
+++ Chat Room Demo/Chat_Room_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_Room_DemoApp.swift
+//  Chat Room Demo
+//
+//  Created by USER on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct Chat_Room_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
